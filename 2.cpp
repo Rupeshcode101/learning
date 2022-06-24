7fac60b8995a243a899f56ff82e55db3c08d4bc2
Rupeshcode101 <108032547+Rupeshcode101@users.noreply.github.com>
2022-06-24 18:34:28 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+using namespace std; //we can define namespace all 4 once here.A namespace is a declarative region that provides a scope to the identifiers 
+                      //(the names of types, functions, variables, etc) inside it
+
+
+int main(){
+std::cout<<"Hello world\n";
+
+cout<<"hello world\n";       //above defination for this line
+
+return 0;
+
+}
